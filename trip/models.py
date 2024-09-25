bfb0d12cc29af11db670c7c737f35f1b25f6f442
Liviu Theodor Ene <eneliviu@gmail.com>
2024-09-25 17:06:33 +0000

    Fix modal trip details for dynamic display, edit and delete

--- trip/models.py
+++ trip/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.validators import MaxValueValidator as mxvv
 # from django.core.validators import ValidationError
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from cloudinary.models import CloudinaryField
 from . utils import get_coordinates
 
@@ -135,6 +137,9 @@ class Comment(AbstractPostModel):
                              related_name='comments')
     approved = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ["created_on"]
+
     def __str__(self):
         return f'Comment by {self.user.username} for \
                             {self.trip.user} on \
@@ -184,3 +189,19 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+# Create user profile when a new user registers
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    '''
+    Create profile for new user automatically
+    '''
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        # User follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+# post_save.connect(create_profile, sender=User)
+
