f6f78a6cacb657adea4214479a0d69065bb86372
Liviu Theodor Ene <eneliviu@gmail.com>
2024-09-28 17:38:48 +0000

    Add input check for trip form on start/dates and strip status

--- trip/forms.py
+++ trip/forms.py
@@ -8,35 +8,35 @@ class AddTripForm(forms.ModelForm):
 
     class Meta:
         model = Trip
-        # fields = ('title', 'country',)
         exclude = ('user', 'lat', 'lon', 'created_on', 'status',)
         widgets = {
             'start_date': forms.widgets.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.widgets.DateInput(attrs={'type': 'date'})
+            'end_date': forms.widgets.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': 3, 'cols': 40})
         }
 
-    # # def clean(self):
-    # #     cleaned_data = super(AddTripForm, self).clean()
-    # #     start_date = cleaned_data.get('start_date')
-    # #     end_date = cleaned_data.get('end_date')
-    # #     if start_date and end_date:
-    # #         if end_date < start_date:
-    # #             self.add_error('end_date', 
-    # #                            'End date cannot be earlier than start date.')        
-    # #     return cleaned_data
-
-    # def clean(self, *args, **kwargs):
-    #     '''
-    #     Add custom validation to ensure that ```end_date``` is not
-    #     prior to ```start_date```
-    #     '''
+    # def clean(self):
     #     cleaned_data = super(AddTripForm, self).clean()
     #     start_date = cleaned_data.get('start_date')
     #     end_date = cleaned_data.get('end_date')
-    #     if start_date and end_date: 
+    #     if start_date and end_date:
     #         if end_date < start_date:
-    #             raise ValidationError({
-    #                     NON_FIELD_ERRORS: ['overlapping date range',],
-    #                 })
-    #     super().clean(*args, **kwargs)
+    #             self.add_error('end_date', 
+    #                            'End date cannot be earlier than start date.')        
+    #     return cleaned_data
 
+    def clean(self, *args, **kwargs):
+        '''
+        Add custom validation to ensure that ```end_date``` is not
+        prior to ```start_date```
+        '''
+        cleaned_data = super(AddTripForm, self).clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        if start_date and end_date:
+            if end_date < start_date:
+                raise ValidationError({
+                        # NON_FIELD_ERRORS: ['End date cannot be earlier than start date',],
+                        'End date cannot be earlier than start date'
+                    })
+        super().clean(*args, **kwargs)
--- trip/views.py
+++ trip/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import (render, redirect,
+                              get_object_or_404, reverse)
 from django.views import generic
-from django.core import serializers
+# from django.core import serializers
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import (Trip, Comment, Note, Image)
 from .forms import AddTripForm
+
+
+def landing_page(request):
+    '''
+    View for Landing page
+    '''
+    # Serialization require a list of objects:
+    trips = list(Trip.objects.filter(shared=True).values())
+
+    return render(request,
+                  'trip/landing_page.html',
+                  {'trips': trips})
 
 
 def user_page(request):
@@ -26,6 +41,15 @@ def user_page(request):
     if request.method == 'GET':
         trips = Trip.objects.filter(user=request.user).prefetch_related('images', 'comments')
         comments_count, images_count = _trip_stats(trips)
+        context = {
+            'trips': trips, 
+            'comments_count': comments_count,
+            'images_count': images_count,
+            'add_trip_form': add_trip_form,
+        }
+        return render(request,
+                      "trip/user_page.html",
+                      context)
        
     if request.method == 'POST':
         if add_trip_form.is_valid():
@@ -34,40 +58,65 @@ def user_page(request):
             add_trip_form.save()
             print('Trip saved')
             
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                f'New trip to {trip.place}, {trip.country} added'
+            )
+            
             # Reload trips and recalculate stats after saving:
             add_trip_form = AddTripForm()
             trips = Trip.objects.filter(user=request.user).prefetch_related('images', 'comments')
             comments_count, images_count = _trip_stats(trips)
-
+            context = {'trips': trips,
+                       'comments_count': comments_count,
+                       'images_count': images_count,
+                       'add_trip_form': add_trip_form,
+                       }
             # Redirect to avoid re-posting the form on refresh
             return redirect('user')
-
-            # messages.add_message(
-            #     request,
-            #     messages.SUCCESS,
-            #     'Trip info succesfully added'
-            # )
-            # trip = list(Trip.objects.values())
-            # return render(request,
-            #               "trip/user_page.html",
-            #               {})
         else:
-            # print(add_trip_form.errors.as_data())
-            # messages.add_message(
-            #     request,
-            #     messages.ERROR,
-            #     form.errors.as_data()
-            # )
-            add_trip_form = AddTripForm()
+            messages.add_message(request,
+                                 messages.ERROR,
+                                 add_trip_form.errors.as_data())
+            
+            # while True:
+            #     add_trip_form = AddTripForm(request.POST or None)
+            #     if add_trip_form.is_valid():
+            #         trip = add_trip_form.save(commit=False)
+            #         trip.user = request.user
+            #         add_trip_form.save()
+            #         print('Trip saved')
+                    
+            #         messages.add_message(
+            #             request,
+            #             messages.SUCCESS,
+            #             f'New trip to {trip.place}, {trip.country} added'
+            #         )
+                    
+            #         # Reload trips and recalculate stats after saving:
+            #         add_trip_form = AddTripForm()
+            #         trips = Trip.objects.filter(user=request.user).prefetch_related('images', 'comments')
+            #         comments_count, images_count = _trip_stats(trips)
+            #         context = {'trips': trips,
+            #                     'comments_count': comments_count,
+            #                     'images_count': images_count,
+            #                     'add_trip_form': add_trip_form,
+            #                     }
+            #         # Redirect to avoid re-posting the form on refresh
+            #         break
+    
+            return redirect('user')
+            # add_trip_form = AddTripForm()
+            # return HttpResponseRedirect(reverse('user'))
+            
+        return render(request,
+                      "trip/user_page.html",
+                      context)
 
-    context = {
-        'trips': trips,
-        'comments_count': comments_count,
-        'images_count': images_count,
-        'add_trip_form': add_trip_form,
-    }
 
-    return render(request, "trip/user_page.html", context)
+
+    
 
 
 def user_registration(request):
@@ -102,53 +151,6 @@ def contact(request):
     '''
     
     return render(request, 'trip/contact_us.html', {})
-
-
-def landing_page(request):
-    '''
-    View for Landing page
-    '''
-
-    if request.method == 'GET':
-        add_trip_form = AddTripForm(request.GET)
-        if Trip.objects.all().last() is None:  # Handle no trip records
-            context = {'add_trip_form': add_trip_form}
-        else:  # Get all the trips  
-            # List the trip values to serialize in Leaflet
-            trips = list(Trip.objects.values().filter(shared=True))
-            # print(trips)
-            context = {'add_trip_form': add_trip_form,
-                       'trips': trips,
-                       }
-        return render(request,
-                      'trip/landing_page.html',
-                      context=context)
-
-    if request.method == 'POST':
-        add_trip_form = AddTripForm(request.POST or None)
-        if add_trip_form.is_valid():
-            trip = add_trip_form.save(commit=False)
-            trip.user = request.user
-            add_trip_form.save()
-            # messages.add_message(
-            #     request,
-            #     messages.SUCCESS,
-            #     'Trip info succesfully added'
-            # )
-            # trip = list(Trip.objects.values())
-            return redirect('trip/landing_page.html')
-        else:
-            # print(add_trip_form.errors.as_data())
-            # messages.add_message(
-            #     request,
-            #     messages.ERROR,
-            #     form.errors.as_data()
-            # )
-            add_trip_form = AddTripForm()
-            return render(request, 
-                          'trip/landing_page.html',
-                          {'add_trip_form': add_trip_form})
-
 
 
 
